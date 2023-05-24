01fe484e42396f3217cd0418351564a6730152b9
Jchips <20979096+Jchips@users.noreply.github.com>
2023-05-24 12:44:07 -0700

    updated README

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,4 @@
 <template>
-  <!-- <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/gen-z-dance-wiki">Gen Z Dance Wiki</router-link>
-  </nav> -->
-  
   <navbar-view />
   <footer-view />
 </template>
