115db3646c5fccd2d9fd1a79df2f2bd792e3c716
Jchips <20979096+Jchips@users.noreply.github.com>
2023-09-07 15:21:01 -0700

    added new show, minor UI improvements to TV page

--- src/views/protected/TV.vue
+++ src/views/protected/TV.vue
@@ -6,6 +6,7 @@
     <div class="container">
       <!-- <aside class="col-sm-2"> -->
       <aside>
+        <div><a href="#A" class="text-secondary">A</a></div>
         <div><a href="#C" class="text-secondary">C</a></div>
         <div><a href="#H" class="text-secondary">H</a></div>
         <div><a href="#M" class="text-secondary">M</a></div>
@@ -30,7 +31,7 @@
                 <p>{{ show.genres }}</p>
                 <img v-for="logo in show.availability" :key="logo" :src="require(`@/assets/tv-logos/${logo}.png`)" :alt="`${logo} logo`"/><br/>
                 <p v-if="show.lgbtq">🏳️‍🌈</p>
-                <span class="img-cred">Image cred: <a :href="show.img_link">{{ show.img_creds }}</a></span>
+                <span class="img-cred">Image cred: <a :href="show.img_link" target="_blank">{{ show.img_creds }}</a></span>
               </div>
             </div>
           </div>
@@ -39,20 +40,20 @@
             <p><span>{{ show.title }}</span> {{ show.description }}</p>
 
             <!-- My Thoughts -->
-            <div class="text-center"><button class="btn btn-danger button" @click="show.toggle_my_thoughts = !show.toggle_my_thoughts">{{ show.toggle_my_thoughts ? 'My Thoughts -' : 'My Thoughts +'}}</button></div>
+            <div class="text-center"><button class="btn button" @click="show.toggle_my_thoughts = !show.toggle_my_thoughts">{{ show.toggle_my_thoughts ? 'My Thoughts -' : 'My Thoughts +'}}</button></div>
             <div v-show="show.toggle_my_thoughts" class="toggled-text">
               <div class="text-center"><img class="rating" :src="show.rating_img"></div>
               <p v-for="paragraph in show.my_thoughts" :key="paragraph">{{ paragraph }}</p>
             </div>
 
             <!-- My Thoughts (with spoilers) -->
-            <div v-if="show.thoughts_with_spoilers" class="text-center"><button class="btn btn-danger button" @click="show.toggle_my_thoughts_with_spoilers = !show.toggle_my_thoughts_with_spoilers">{{ show.toggle_my_thoughts_with_spoilers ? `My Thoughts (with spoilers) -` : 'My Thoughts (with spoilers) +'}}</button></div>
+            <div v-if="show.thoughts_with_spoilers" class="text-center"><button class="btn button" @click="show.toggle_my_thoughts_with_spoilers = !show.toggle_my_thoughts_with_spoilers">{{ show.toggle_my_thoughts_with_spoilers ? `My Thoughts (with spoilers) -` : 'My Thoughts (with spoilers) +'}}</button></div>
             <div v-if="show.thoughts_with_spoilers" v-show="show.toggle_my_thoughts_with_spoilers" class="toggled-text">
               <p v-for="paragraph in show.thoughts_with_spoilers" :key="paragraph">{{ paragraph }}</p>
             </div>
 
             <!-- TV sins -->
-            <div v-if="show.fav_episodes" class="text-center"><button class="btn btn-danger button" @click="show.toggle_fav_episodes = !show.toggle_fav_episodes">{{ show.toggle_fav_episodes ? 'Fav Episodes -' : 'Fav Episodes +'}}</button></div>
+            <div v-if="show.fav_episodes" class="text-center"><button class="btn button" @click="show.toggle_fav_episodes = !show.toggle_fav_episodes">{{ show.toggle_fav_episodes ? 'Fav Episodes -' : 'Fav Episodes +'}}</button></div>
             <div v-if="show.fav_episodes">
               <ul v-show="show.toggle_fav_episodes" class="toggled-text">
                 <li v-for="episode in show.fav_episodes" :key="episode">{{ episode }}</li>
@@ -60,7 +61,7 @@
             </div>
 
             <!-- TV sins -->
-            <div v-if="show.tv_sins" class="text-center"><button class="btn btn-danger button" @click="show.toggle_tv_sins = !show.toggle_tv_sins">{{ show.toggle_tv_sins ? 'TV Sins (SPOILERS...duh) -' : 'TV Sins (SPOILERS...duh) +'}}</button></div>
+            <div v-if="show.tv_sins" class="text-center"><button class="btn button" @click="show.toggle_tv_sins = !show.toggle_tv_sins">{{ show.toggle_tv_sins ? 'TV Sins (SPOILERS...duh) -' : 'TV Sins (SPOILERS...duh) +'}}</button></div>
             <div v-if="show.tv_sins">
               <ol v-show="show.toggle_tv_sins" class="toggled-text">
                 <li v-for="sin in show.tv_sins" :key="sin">{{ sin }}</li>
